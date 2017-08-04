8f8c50b39068f4d5b405337ac17c548a0d0acf51
ManuNandhan <manunandhanmn@gmail.com>
2017-08-04 09:43:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,6 +105,12 @@ var htmlTemplate = `
     </html>`;
     return htmlTemplate;
 }
+var counter = 0;
+app.get('/counter', function(req, res){
+    counter++;
+    res.send(counter.toString());
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
