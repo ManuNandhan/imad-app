a01c49591d8d4c717f38cc4ce5eb7e5782cbbd81
ManuNandhan <manunandhanmn@gmail.com>
2017-08-04 09:43:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ var counter = 0;
 app.get('/counter', function(req, res){
     counter++;
     res.send(counter.toString());
-}
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
