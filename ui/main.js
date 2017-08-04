4ae68f8716f11a22f8541cd1d68f35d32eb09890
ManuNandhan <manunandhanmn@gmail.com>
2017-08-04 10:17:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,12 +16,13 @@
 // };
 
 //counte code
- var counter = 0;
+ 
  var button = document.getElementById('counter');
+ var counter = 0;
  
  button.onclick = function(){
      
-     counter = counter++;
+     counter = counter + 1;
      var span = document.getElementById('count');
      span.innerHTML = counter.toString();
  }
