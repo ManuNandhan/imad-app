5d69ce7697703fe7ee720b21693737c0bf5e16fb
ManuNandhan <manunandhanmn@gmail.com>
2017-08-04 10:13:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,27 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'MANU';
+// console.log('Loaded!');
+// var element = document.getElementById('main-text');
+// element.innerHTML = 'MANU';
 
-var img = document.getElementById('madi');
+// var img = document.getElementById('madi');
 
-var marginLeft = 0;
-function moveRight(){
-    marginLeft +=1;
-    img.style.marginLeft =  marginLeft + 'px';
-}
-img.onclick = function(){
+// var marginLeft = 0;
+// function moveRight(){
+//     marginLeft +=1;
+//     img.style.marginLeft =  marginLeft + 'px';
+// }
+// img.onclick = function(){
     
-    var interval = setInterval(moveRight, 50);
-     //img.style.marginLeft = "100px";
-};
+//     var interval = setInterval(moveRight, 50);
+//      //img.style.marginLeft = "100px";
+// };
 
+//counte code
+ var button = document.getElementById('counter');
+ 
+ button.onclick = function(){
+     
+     counter = counter++;
+     var span = document.getElementById('count');
+     span.innerHTML = counter.toString();
+ }
+ 
